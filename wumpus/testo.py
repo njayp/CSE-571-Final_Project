a3d40fc933bf60810e1fdba56c87e079dc846d95
nick powell <nickjaypowell@gmail.com>
2020-04-30 12:24:29 -0700

    init wumpus

---
+++ wumpus/testo.py
@@ -0,0 +1,10 @@
+from wumpus_kb import *
+
+#print axiom_generator_percept_sentence(0, [False, True, False, False, True])
+#print axiom_generator_initial_location_assertions(0, 1)
+#print axiom_generator_pits_and_breezes(1, 1, 1, 2, 1, 2)
+#print axiom_generator_wumpus_and_stench(1, 1, 1, 2, 1, 2)
+#print axiom_generator_at_least_one_wumpus(1, 3, 1, 2)
+#print axiom_generator_at_most_one_wumpus(1, 3, 1, 2)
+#print axiom_generator_only_in_one_location(1, 1, 1, 3, 1, 2, 5)
+print axiom_generator_only_one_action_axioms(2)
