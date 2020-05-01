b006c9a99b151ad570e04e0bd4ae638aa900f3ce
Omkar <omkarpopalghat1729@gmail.com>
2020-05-01 12:20:22 -0700

    partial q-learning model

--- wumpus/q_learning_agent.py
+++ wumpus/q_learning_agent.py
@@ -10,6 +10,7 @@ from wumpus_environment import Explorer
 import pickle
 import sys
 import random
+import copy
 #from time import clock
 #import sys
 #import utils
@@ -19,7 +20,7 @@ class QLearningAgent(Explorer):
         super(QLearningAgent, self).__init__(self.agent_program, heading, environment, verbose)
         self.heading = heading
         self.QValues = {}
-        self.values = []
+        self.score = 0
         self.step = 0
         self.epsilon = 0.05
         self.gamma = 0.8
@@ -28,6 +29,7 @@ class QLearningAgent(Explorer):
         self.flag = True
         self.action_list = ['TurnRight', 'TurnLeft', 'Forward', 'Climb', 'Shoot', 'Stop', 'Grab']
         self.prev_action = None
+        self.prev_state = None
         
     
     def reset(self):
@@ -36,37 +38,31 @@ class QLearningAgent(Explorer):
         self.values = []
         self.QValues = {}
         self.flag = True
+        self.write_q_values(self.QValues)
         
         
     def agent_program(self, percept):
-        action_list = ['TurnRight', 'TurnLeft', 'Forward', 'Climb', 'Shoot', 'Stop', 'Grab']
-        self.values.append(self.performance_measure)
-#        if self.flag:
-#            self.run_iterations(percept, self.initial_location, action_list)
+#        self.values.append(self.performance_measure)
         state = (tuple(percept), self.location)
-        for action in action_list:
+        self.load_q_values()
+        for action in self.action_list:
             self.add_state(state, action)
+        self.write_q_values(self.QValues)
         self.update_q_values(state)
         return_act = self.choose_action(state)
         
         if self.verbose:
             print("Q-Value iteration : step no. = {0}".format(self.step))
-        print("Choosing action : {0}".format(self.QValues.keys()[1][1]))
+        print("Choosing action : {0}".format(return_act))
         self.prev_action = return_act
+        self.prev_state = state
+        self.score = self.performance_measure
+        self.step+=1
         return return_act
         
     def add_state(self, state, action):
-        file_name = 'q_value_dict.pickle'
-        self.QValues = self.load_q_values()
         if (state, action) not in self.QValues.keys():
             self.QValues[(state, action)] = 0
-        try:
-            with open(file_name, "wb") as file:
-                pickle.dump(self.QValues, file)
-        except:
-            print("Uanble to write to Dictionary")
-            sys.exit(-1)
-        
             
     def load_q_values(self):
         file_name = 'q_value_dict.pickle'
@@ -76,14 +72,18 @@ class QLearningAgent(Explorer):
         except:
             print("Dictionary cannot be opened")
             sys.exit(-1)
+        if self.QValues == None:
+            self.QValues = {}
             
     def update_q_values(self, state):
+        if self.prev_state == None and self.prev_action == None:
+            return
         updated_val = 0
-        legal_actions = self.get_legal_actions(state)
-        q_max = max([ self.QValue(state, action) for action in legal_actions])
-        updated_val = self.values[-1] + self.discount * q_max
-        self.QValues[(state, self.prev_action)] = self.QValues[(state, self.prev_action)] + self.alpha * (updated_val - self.QValues[(state, self.prev_action)])
-        self.write_q_values(self.QValue)
+#        legal_actions = self.get_legal_actions(state)
+        q_max = max([ self.QValues[(state, action)] for action in self.action_list])
+        updated_val = self.get_reward() + self.discount * q_max
+        self.QValues[(self.prev_state, self.prev_action)] = self.QValues[(state, self.prev_action)] + self.alpha * (updated_val - self.QValues[(self.prev_state, self.prev_action)])
+        self.write_q_values(self.QValues)
             
         
     def get_legal_actions(self, state):
@@ -92,14 +92,19 @@ class QLearningAgent(Explorer):
             res.remove('Climb')
         return res
         
-    def choose_action(self):
+    def choose_action(self, state):
         choice = random.randint(1,10)
+        key_list = [s for s, a in self.QValues.items() if s[0] == state]
+        max_val_list = [(self.QValues[key], key) for key in key_list]
+        max_val_list = sorted(max_val_list, key = lambda x:x[0])
         if choice > 2:
-            return self.QValues.keys()[0][1]
+            return max_val_list[-1][1][1]
         else:
-            return random.choice(self.action_list.copy().remove(self.QValues.keys()[0][1]))
+            list_copy = ['TurnRight', 'TurnLeft', 'Forward', 'Climb', 'Shoot', 'Stop', 'Grab']
+            list_copy.remove(max_val_list[-1][1][1])
+            return random.choice(list_copy)
         
-    def write_q_values(q_val):
+    def write_q_values(self,q_val):
         file_name = 'q_value_dict.pickle'
         try:
             with open(file_name, "wb") as file:
@@ -108,4 +113,6 @@ class QLearningAgent(Explorer):
             print("Uanble to write to Dictionary")
             sys.exit(-1)
         
+    def get_reward(self):
+        return self.performance_measure - self.score
         
